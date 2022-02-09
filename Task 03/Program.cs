c4a6c2e8e525cc9464eb0da70318ea620b88c03c
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2022-02-10 02:21:15 +0300

    added task 03

---
+++ Task 03/Program.cs
@@ -0,0 +1,13 @@
+﻿// Даны два числа. Показать большее и меньшее число
+int number1, number2;
+Console.WriteLine("Введите первое число :");
+number1=Int32.Parse(Console.ReadLine());
+Console.WriteLine("Введите второе число :");
+number2=Int32.Parse(Console.ReadLine());
+if(number1>number2)
+    Console.WriteLine("Первое число {0,5} больше второго {1,5}", number1 ,number2 );
+    else
+    if(number2>number1)
+    Console.WriteLine("Второе число {0,5} больше первого {1,5}", number2 ,number1 );
+        else
+        Console.WriteLine("Второе число {0,5} равно первому {1,5}", number2 ,number1 );
