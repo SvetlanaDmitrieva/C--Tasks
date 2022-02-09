8b96c46b3ddff9bac772898bda932773f75c6674
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2022-02-10 00:57:00 +0300

    add task 01

---
+++ Task 01/Program.cs
@@ -0,0 +1,8 @@
+﻿// 1.	Вывести квадрат числаn
+string str;
+int number, number2;
+Console.WriteLine("Введите число :");
+str = Console.ReadLine();
+number=Int32.Parse(str);
+number2=number*number;
+System.Console.WriteLine("Квадрат числа {0} равен {1}", number ,number2 );
