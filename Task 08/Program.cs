a40f2aeba0c34b6da61716845207bb4762c280bc
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2022-02-14 02:08:46 +0300

    added task 08

---
+++ Task 08/Program.cs
@@ -0,0 +1,9 @@
+﻿// 8.	Показать числа от -N до N
+Console.WriteLine("Введите любое  целое число :");
+//
+int number1;
+//
+number1=Math.Abs(Int32.Parse(Console.ReadLine()));
+
+for(int i=(-number1);i<=number1;i++)
+ Console.Write("{0} ", i);
