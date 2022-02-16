ff1f6044416a92e00678779d29f34e645c4c50d8
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2022-02-17 02:25:24 +0300

    corrected task 22

--- Task 22/Program.cs
+++ Task 22/Program.cs
@@ -1,5 +1,5 @@
 ﻿// 22.	Найти расстояние между точками в пространстве 2D/3D
-//Console.WriteLine("Hello, World!");
+
 double []coordinateX = new double[2];
 double []coordinateY = new double[2];
 double []coordinateZ = new double[2];
@@ -14,6 +14,6 @@ for ( int i=0; i<2; i++){
      coordinateZ[i]=Double.Parse(Console.ReadLine());
  }
 distance= Math.Sqrt(Math.Pow((coordinateX[0]-coordinateX[1]),2)+Math.Pow((coordinateY[0]-coordinateY[1]),2)+Math.Pow((coordinateZ[0]-coordinateZ[1]),2));
- System.Console.WriteLine("Расстояние между точками с указанными координатами равно {0}",distance );
+ Console.WriteLine("Расстояние между точками с указанными координатами равно {0}",distance );
 
 
