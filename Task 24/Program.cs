64538be7175f385200307e05e10edede742228d2
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2022-02-18 01:04:30 +0300

    added task 24

---
+++ Task 24/Program.cs
@@ -0,0 +1,25 @@
+﻿// 24.	Найти кубы чисел от 1 до N
+int numberMax,counterNumber=0;
+Console.WriteLine("Введите любое число :");
+numberMax=Int32.Parse(Console.ReadLine());
+Console.WriteLine("    Number  Number**3 Number  Number**3 Number  Number**3 ");
+for(int i=1; i<=numberMax; i++)
+{
+Console.Write("{0,9}{1,9}",i ,i*i*i);
+counterNumber++;
+if(counterNumber%3==0){Console.WriteLine();}
+if(counterNumber%12==0){Console.WriteLine();}
+}
+
+
+//Console.WriteLine("Hello, World!");
+/*
+Console.WriteLine("Введите любое число :");
+numberMax=Int32.Parse(Console.ReadLine());
+Console.WriteLine("    Number  Number**2 Number  Number**2 Number  Number**2 ");
+for(int i=1; i<=numberMax; i++)
+{
+Console.Write("{0,9}{1,9}",i ,i*i);
+counterNumber++;
+if(counterNumber%3==0){Console.WriteLine();}
+}*/
