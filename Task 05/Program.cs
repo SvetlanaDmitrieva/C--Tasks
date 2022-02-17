d4f98e1e95606a4320e689732044649a20f7cdd3
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2022-02-18 02:30:54 +0300

    corrected task 05

--- Task 05/Program.cs
+++ Task 05/Program.cs
@@ -1,23 +1,26 @@
 ﻿// 5.	Найти максимальное из трех чисел
-int [] arrayNumber= new int [3];
-int maxNumber=0, Flag=0;
+int[] arrayNumber = new int[3];
+int maxNumber = 0, Flag = 0;
 
 for (int i = 0; i < arrayNumber.Length; i++)
 {
-    Console.WriteLine("Введите {0}-е число :",(i+1));
-    arrayNumber[i]=Int32.Parse(Console.ReadLine());
-    if(i==0) {maxNumber= arrayNumber[i];
-        Flag=i+1;
-    } 
+    Console.WriteLine("Введите {0}-е число :", (i + 1));
+    arrayNumber[i] = Int32.Parse(Console.ReadLine());
+    if (i == 0)
+    {
+        maxNumber = arrayNumber[i];
+        Flag = i + 1;
+    }
     else
     {
-        if( maxNumber < arrayNumber[i]) {
-            maxNumber= arrayNumber[i];
-           Flag=i+1;
-            } 
-    };   
-    
-    }
-Console.WriteLine("{0}-е число {1} является максимальным из введенных", Flag, maxNumber );
-    
+        if (maxNumber < arrayNumber[i])
+        {
+            maxNumber = arrayNumber[i];
+            Flag = i + 1;
+        }
+    };
+
+}
+Console.WriteLine("{0}-е число {1} является максимальным из введенных", Flag, maxNumber);
+
 
