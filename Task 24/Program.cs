883eeb14e9df31357d8581de46b76b43148ea2f0
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2022-02-18 01:30:15 +0300

    Corrected task 24

--- Task 24/Program.cs
+++ Task 24/Program.cs
@@ -10,16 +10,3 @@ counterNumber++;
 if(counterNumber%3==0){Console.WriteLine();}
 if(counterNumber%12==0){Console.WriteLine();}
 }
-
-
-//Console.WriteLine("Hello, World!");
-/*
-Console.WriteLine("Введите любое число :");
-numberMax=Int32.Parse(Console.ReadLine());
-Console.WriteLine("    Number  Number**2 Number  Number**2 Number  Number**2 ");
-for(int i=1; i<=numberMax; i++)
-{
-Console.Write("{0,9}{1,9}",i ,i*i);
-counterNumber++;
-if(counterNumber%3==0){Console.WriteLine();}
-}*/
