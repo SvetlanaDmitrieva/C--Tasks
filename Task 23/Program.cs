26e8b9c9507e67101d3fbcb65a8363ca8c86765a
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2022-02-18 00:41:52 +0300

    corrected task 23

---
+++ Task 23/Program.cs
@@ -0,0 +1,12 @@
+﻿// 23.	Показать таблицу квадратов чисел от 1 до N
+int numberMax,counterNumber=0;
+
+Console.WriteLine("Введите любое число :");
+numberMax=Int32.Parse(Console.ReadLine());
+Console.WriteLine("    Number  Number**2 Number  Number**2 Number  Number**2 ");
+for(int i=1; i<=numberMax; i++)
+{
+Console.Write("{0,9}{1,9}",i ,i*i);
+counterNumber++;
+if(counterNumber%3==0){Console.WriteLine();}
+}
