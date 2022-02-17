88a897b476fa3c070a477a1b56a6f36a1f765277
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2022-02-18 02:32:16 +0300

    corrected task 06

--- Task 06/Program.cs
+++ Task 06/Program.cs
@@ -1,16 +1,16 @@
 ﻿// 6.	Написать программу вычисления значения функции y = f(a)
 
 int numMax(int a, int b, int c)
-{ 
-   int maxNum=a;
-   if (b>maxNum) maxNum= b;
-   if (c>maxNum) maxNum= c;
-return maxNum;
+{
+    int maxNum = a;
+    if (b > maxNum) maxNum = b;
+    if (c > maxNum) maxNum = c;
+    return maxNum;
 }
-int [] aNum= new int [3];
+int[] aNum = new int[3];
 for (int i = 0; i < aNum.Length; i++)
 {
-    Console.WriteLine("Введите {0}-е число :",(i+1));
-    aNum[i]=Int32.Parse(Console.ReadLine());
+    Console.WriteLine("Введите {0}-е число :", (i + 1));
+    aNum[i] = Int32.Parse(Console.ReadLine());
 }
-Console.WriteLine("Число {0} является максимальным из введенных", numMax(aNum[0], aNum[1] ,aNum[2]) );
+Console.WriteLine("Число {0} является максимальным из введенных", numMax(aNum[0], aNum[1], aNum[2]));
