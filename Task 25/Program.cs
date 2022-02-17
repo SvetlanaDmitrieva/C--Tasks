ba1b875ab0b5d313ccc4d579966e57586e6a91ee
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2022-02-18 01:27:07 +0300

    added task 25

---
+++ Task 25/Program.cs
@@ -0,0 +1,14 @@
+﻿// 25.	Найти сумму чисел от 1 до А
+int numberA = 0, flag = 0, sumNumber = 0;
+while (numberA <= 0)
+{
+    Console.WriteLine("Введите любое положительное число А:");
+    numberA = Int32.Parse(Console.ReadLine());
+    flag++;
+    if (flag == 5) { Console.WriteLine("Вы тспользовали 5 возможных попыток"); return; }
+}
+for (int i = 1; i <= numberA; i++)
+{
+    sumNumber = sumNumber + i;
+}
+Console.WriteLine("Сумма чисел от 1 до {0} равна {1}", numberA, sumNumber);
