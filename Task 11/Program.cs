f2038e9b9f2ea4049dac9ac3db442eaee05c5165
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2022-02-18 11:03:38 +0300

    formatted task 11

--- Task 11/Program.cs
+++ Task 11/Program.cs
@@ -1,16 +1,18 @@
 ﻿//11.	Показать вторую цифру трёхзначного числа
 
-int Num,Num1,Flag=0;
+int Num, Num1, Flag = 0;
 Console.WriteLine("Введите любое положительное трехзначное число :");
-Num=Int32.Parse(Console.ReadLine());
-while(Num < 100 || Num > 999) {
+Num = Int32.Parse(Console.ReadLine());
+while (Num < 100 || Num > 999)
+{
     Flag++;
-    Console.WriteLine("Введеное число должно быть положительным трехзначным .Попытка {0} из 5-ти", Flag+1);
-    if(Flag>=5) {
+    Console.WriteLine("Введеное число должно быть положительным трехзначным .Попытка {0} из 5-ти", Flag + 1);
+    if (Flag >= 5)
+    {
         Console.WriteLine("Вы использовали 5 попыток. Выход из программы");
         return;
     }
-    Num=Int32.Parse(Console.ReadLine());
+    Num = Int32.Parse(Console.ReadLine());
 }
-Num1=Num/10;
-Console.Write("Вторая цифра введенного трехзначного числа {0} - {1}",Num,(Num1%10) );
+Num1 = Num / 10;
+Console.Write("Вторая цифра введенного трехзначного числа {0} - {1}", Num, (Num1 % 10));
