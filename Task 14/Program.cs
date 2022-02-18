604b642c3ceaed363364c390c25309da900384d1
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2022-02-18 11:11:05 +0300

    formatted task 14

--- Task 14/Program.cs
+++ Task 14/Program.cs
@@ -1,13 +1,13 @@
 ﻿// 14.	Выяснить, кратно ли число заданному, если нет, вывести остаток.
 
-int Num1,Num2,Rest;
+int Num1, Num2, Rest;
 Console.WriteLine("Введите первое число :");
-Num1=Int32.Parse(Console.ReadLine());
+Num1 = Int32.Parse(Console.ReadLine());
 Console.WriteLine("Введите второе число :");
-Num2=Int32.Parse(Console.ReadLine());
-Rest=Num1%Num2;
-if(Rest==0) Console.WriteLine("Первое число {0}  кратно второму числу {1}", Num1,Num2); 
-else 
+Num2 = Int32.Parse(Console.ReadLine());
+Rest = Num1 % Num2;
+if (Rest == 0) Console.WriteLine("Первое число {0}  кратно второму числу {1}", Num1, Num2);
+else
 {
-    Console.WriteLine("Первое число {0}  не кратно второму числу {1}, остаток от деления - {2}", Num1,Num2,Rest);
+    Console.WriteLine("Первое число {0}  не кратно второму числу {1}, остаток от деления - {2}", Num1, Num2, Rest);
 }
