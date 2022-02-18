f93477f969f3d8049b57f0986e0f894e38e9cac1
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2022-02-18 10:58:45 +0300

    corrected task 07

--- task 07/Program.cs
+++ task 07/Program.cs
@@ -1,8 +1,12 @@
-﻿// 7.	Выяснить является ли число чётным
+﻿/// 7.	Выяснить является ли число чётным
 Console.WriteLine("Введите любое число :");
-string str;
 int number1;
-str = Console.ReadLine();
-number1=Int32.Parse(str);
-if (number1%2==0) Console.WriteLine("Число {0} является четным", number1);
-    else Console.WriteLine("Число {0} является нечетным", number1);
+number1 = Int32.Parse(Console.ReadLine());
+if (number1 % 2 == 0)
+{
+    Console.WriteLine("Число {0} является четным", number1);
+}
+else
+{
+    Console.WriteLine("Число {0} является нечетным", number1);
+}
