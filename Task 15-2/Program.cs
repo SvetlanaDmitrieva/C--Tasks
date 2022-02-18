71916afbb6b16861d9680306e86c8c5567145798
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2022-02-18 11:15:25 +0300

    formatted task 15-2

--- Task 15-2/Program.cs
+++ Task 15-2/Program.cs
@@ -2,14 +2,14 @@
 
 int Num;
 Console.WriteLine("Введите любое число :");
-Num=Int32.Parse(Console.ReadLine());
-if(Num%7==0) {Console.Write("Введеное число {0} кратно 7",Num);}
-    else
-    {
-       Console.Write("Введеное число {0} не кратно 7",Num); 
-    }
-if(Num%23==0) {Console.Write(" и кратно 23");}
-    else
-    {
-       Console.Write(" и не кратно 23"); 
-    }
+Num = Int32.Parse(Console.ReadLine());
+if (Num % 7 == 0) { Console.Write("Введеное число {0} кратно 7", Num); }
+else
+{
+    Console.Write("Введеное число {0} не кратно 7", Num);
+}
+if (Num % 23 == 0) { Console.Write(" и кратно 23"); }
+else
+{
+    Console.Write(" и не кратно 23");
+}
