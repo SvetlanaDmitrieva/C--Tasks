76dc0b84f7cc96fd84758380d2198dee583fd3ba
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2022-02-19 22:59:30 +0300

    formatted task 18

--- Task 18/Program.cs
+++ Task 18/Program.cs
@@ -1,15 +1,15 @@
 ﻿// 18.	Проверить истинность утверждения ¬(X ⋁ Y) = ¬X ⋀ ¬Y
-
-bool [] arrayX={false,true};
-bool [] arrayY={false,true};
+bool[] arrayX = { false, true };
+bool[] arrayY = { false, true };
 bool firstBool, secondBool;
-
 Console.WriteLine("    X      Y   !(X||Y)   !X&&!Y ");
-for (int i=0;i<2; i++){
-    for (int j=0; j<2 ; j++){
-        firstBool=!(arrayX[i]||arrayY[j]);
-        secondBool=!(arrayX[i]||arrayY[j]);
-Console.WriteLine("{0,7}{1,7}{2,8}{3,8}",arrayX[i],arrayY[j],firstBool,secondBool);
-Console.WriteLine();
+for (int i = 0; i < 2; i++)
+{
+    for (int j = 0; j < 2; j++)
+    {
+        firstBool = !(arrayX[i] || arrayY[j]);
+        secondBool = !(arrayX[i] || arrayY[j]);
+        Console.WriteLine("{0,7}{1,7}{2,8}{3,8}", arrayX[i], arrayY[j], firstBool, secondBool);
+        Console.WriteLine();
     }
 }
