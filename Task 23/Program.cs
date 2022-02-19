54347ee48156d99b1af049658278875421bcdb84
Dmitrieva Svetlana <sv-dmitrieva@list.ru>
2022-02-20 00:03:45 +0300

    formatted task 23

--- Task 23/Program.cs
+++ Task 23/Program.cs
@@ -1,12 +1,11 @@
 ﻿// 23.	Показать таблицу квадратов чисел от 1 до N
-int numberMax,counterNumber=0;
-
+int numberMax, counterNumber = 0;
 Console.WriteLine("Введите любое число :");
-numberMax=Int32.Parse(Console.ReadLine());
+numberMax = Int32.Parse(Console.ReadLine());
 Console.WriteLine("    Number  Number**2 Number  Number**2 Number  Number**2 ");
-for(int i=1; i<=numberMax; i++)
+for (int i = 1; i <= numberMax; i++)
 {
-Console.Write("{0,9}{1,9}",i ,i*i);
-counterNumber++;
-if(counterNumber%3==0){Console.WriteLine();}
+    Console.Write("{0,9}{1,9}", i, i * i);
+    counterNumber++;
+    if (counterNumber % 3 == 0) { Console.WriteLine(); }
 }
